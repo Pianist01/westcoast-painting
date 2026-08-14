8c0e6413f461655b539b3cf1321d9bd0490760bf
Steven Peralta <155492295+Pianist01@users.noreply.github.com>
2026-08-14 19:26:47 +0000

    Fixing Observer

--- about.js
+++ about.js
@@ -18,56 +18,72 @@ const circleTwoOptions = {
 
 let previousScrollY = window.scrollY;
 let scrollingUp;
-let scrollDirection;
+let scrollDirection = 'Down';
+let currentScrollY;
+let atTop;
+
 
 window.addEventListener('scroll', () => {
-    const currentScrollY = window.scrollY;
-    if(currentScrollY > previousScrollY) {
-        console.log('Scrolling down');
-        scrollingUp = false; 
-        console.log(scrollingUp);
-        scrollDirection = currentScrollY;
+            currentScrollY = window.scrollY;
+        if(currentScrollY > previousScrollY) {
+        // console.log('Scrolling down');
+        scrollDirection = 'Down';
+        // console.log(scrollDirection);
     } else if(currentScrollY < previousScrollY) {
-        console.log('Scrolling Up');
-        scrollingUp = true;
-        console.log(scrollingUp);
-        scrollDirection = currentScrollY;
+        // console.log('Scrolling Up');
+        scrollDirection = 'Up';
+        // console.log(scrollDirection);
     }
-    previousScrollY = currentScrollY;
-});
-
-
+        previousScrollY = currentScrollY;
+        // console.log('Current Scroll at:', currentScrollY);
+        if(currentScrollY === 0) {
+            console.log('Y is at 0');
+            atTop = true;
+            scrollDirection = 'Down';
+            console.log('Y is at 0 and scroll direction is:', scrollDirection);
+        }
+    });
 
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
 
-              
-              if(entry.isIntersecting) {
-                if(scrollingUp === false) {
-                    const yearTitle = entry.target.querySelector('h3');
+        // console.log('target:', entry.target);
+        // console.log('intersecting', entry.isIntersecting);
+        // console.log('direction', scrollDirection);
+
+        window.onload = () => {
+            entry.isIntersecting;
+            scrollDirection = 'Down';
+            currentScrollY;
+        }
+
+        if(scrollDirection === 'Down') {
+            if(entry.isIntersecting) {
+                console.log('Observer fired for:', entry.target);
+                 const yearTitle = entry.target.querySelector('h3');
                     yearTitle.style.opacity = '1';
                     yearTitle.classList.add('active');
                     const yearDescription = entry.target.querySelector('p');
                     yearDescription.style.opacity = '1';
                     yearDescription.classList.add('active');
-                }
-            } else {
-                if(scrollingUp === true) {
-                    entry.target.querySelector('h3').style.opacity = '0';
-                    entry.target.querySelector('p').style.opacity = '0';
-                }
+            }
+        } else if(scrollDirection === 'Up') {
+            if(entry.isIntersecting) {
+                entry.target.querySelector('h3').style.opacity = '0';
+                entry.target.querySelector('p').style.opacity = '0';
+            }
         }
     });
 }, options);
 
 const lineObserver = new IntersectionObserver((entries) => {
-    console.log('Observer Fired');
+    // console.log('Observer Fired');
     entries.forEach((entry) => {
         if(entry.isIntersecting) {
-            console.log(entry.target);
-            console.log(entry.isIntersecting);
-            console.log(entry.intersectionRatio);
+            // console.log(entry.target);
+            // console.log(entry.isIntersecting);
+            // console.log(entry.intersectionRatio);
             entry.target.style.backgroundColor = '#333333';
         }
     });
@@ -94,7 +110,7 @@ timeLineItems.forEach((item) => {
 });
 
 rectangle.forEach((item) => {
-    console.log('Observing:', item);
+    // console.log('Observing:', item);
     lineObserver.observe(item);
 });
 
