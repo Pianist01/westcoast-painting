5814fdfca8216ebbfb24a4066825f67965d9fc8d
Steven Peralta <peralta.steven01@gmail.com>
2026-08-20 19:43:02 -0700

    Working on EmailJS codee.

--- contact.js
+++ contact.js
@@ -1,1 +1,22 @@
-console.log('This works');
+ emailjs.init({
+      publicKey: "yqZixuN23JZWME2M1"
+    });
+
+// EMAIL JS SCRIPT
+
+  // GRAB FORM
+
+const form = document.getElementById('contact-form');
+
+function submitForm() {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_utp3liq', 'template_65p3vk9', form).then(() => {
+      console.log('Success, message sent');
+    }, (error) => {
+      console.log('Error, message not sent');
+    });
+  });
+}
+
+submitForm();
